5ef2a57adb19918088f61318ef6d80ab899a7171
Dorian Burihabwa <dorian.burihabwa@sonarsource.com>
2021-11-03 17:40:10 +0100

    Change jar check

--- ws_scan.sh
+++ ws_scan.sh
@@ -1,10 +1,25 @@
 #! /usr/bin/env bash
 
 readonly SCRIPT_DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+readonly MODULES=(
+  "docs/java-custom-rules-example"
+  "external-reports"
+  "its/plugin/tests"
+  "its/plugin/plugins/java-extension-plugin"
+  "its/ruling"
+  "java-checks"
+  "java-checks-testkit"
+  "java-frontend"
+  "java-jsp"
+  "java-surefire"
+  "java-symbolic-execution"
+  "jdt"
+  "sonar-java-plugin"
+)
 
 readonly UNIFIED_AGENT_JAR="wss-unified-agent.jar"
-readonly UNIFIED_AGENT_JAR_MD5_CHECKSUM="706694E349EA14CB04C4621B70D99A93" # MD5 hash for version 29.9.1.1
 readonly UNIFIED_AGENT_JAR_URL="https://unified-agent.s3.amazonaws.com/wss-unified-agent.jar"
+readonly WHITESOURCE_SIGNATURE='Signed by "CN=whitesource software inc, O=whitesource software inc, STREET=79 Madison Ave, L=New York, ST=New York, OID.2.5.4.17=10016, C=US"'
 
 readonly JDK_11="./jdk-11/"
 
@@ -22,18 +37,14 @@ get_unified_agent() {
     exit 1
   fi
 
-  # Verify JAR checksum
-  local checksum="$(md5sum ${UNIFIED_AGENT_JAR} | cut --delimiter=" " --fields=1 | awk ' { print toupper($0) }')"
-  if [[ "${checksum}" != "${UNIFIED_AGENT_JAR_MD5_CHECKSUM}" ]]; then
-    echo "MD5 checksum mismatch." >&2
-    echo "expected: ${UNIFIED_AGENT_JAR_MD5_CHECKSUM}" >&2
-    echo "computed: ${checksum}" >&2
+  # Verify JAR signature
+  local path_to_verification_output="./jarsigner-output.txt"
+  if ! jarsigner -verify -verbose "${UNIFIED_AGENT_JAR}" > "${path_to_verification_output}" ; then
+    echo "Could not verify jar signature" >&2
     exit 2
   fi
-
-  # Verify JAR signature
-  if ! jarsigner -verify "${UNIFIED_AGENT_JAR}"; then
-    echo "Could not verify jar signature" >&2
+  if [[ $(grep --count "${WHITESOURCE_SIGNATURE}" "${path_to_verification_output}") -ne 1 ]]; then
+    echo "Could not find signature line in verification output" >&2
     exit 3
   fi
 }
@@ -44,7 +55,7 @@ local_maven_expression() {
 
 get_product_name() {
   local property="project.name"
-  if which maven_expression >&2 2>/dev/null; then
+  if command -v maven_expression >&2 2>/dev/null; then
     maven_expression "${property}"
   else
     local_maven_expression "${property}"
@@ -53,7 +64,7 @@ get_product_name() {
 
 get_project_version() {
   local property="project.version"
-  if which maven_expression >&2 2>/dev/null; then
+  if command -v maven_expression >&2 2>/dev/null; then
     maven_expression "${property}"
   else
     local_maven_expression "${property}"
@@ -71,11 +82,13 @@ download_jdk_11() {
 scan_module() {
   local folder="${1}"
   local version="${2}"
-  local module_name=$(basename "${1}")
+  local module_name
+  module_name=$(basename "${1}")
   if [[ ! -d "${JDK_11}" ]]; then
     download_jdk_11
   fi
-  export JAVA_HOME="$(readlink -f ${JDK_11})"
+  JAVA_HOME="$(readlink -f ${JDK_11})"
+  export JAVA_HOME
   local app_path="${folder}/target/${module_name}-${version}.jar"
   if [[ -f "${app_path}" ]]; then
     java --illegal-access=warn -jar wss-unified-agent.jar -c whitesource.properties -appPath "${app_path}" -d "${folder}"
@@ -86,26 +99,18 @@ scan_module() {
 }
 
 scan() {
-  export WS_PRODUCTNAME=$(get_product_name)
+  WS_PRODUCTNAME=$(get_product_name)
+  export WS_PRODUCTNAME
   if [[ -z "${PROJECT_VERSION}" ]]; then
     PROJECT_VERSION=$(get_project_version)
   fi
   export WS_PROJECTNAME="${WS_PRODUCTNAME} ${PROJECT_VERSION%.*}"
   echo "${WS_PRODUCTNAME} - ${WS_PROJECTNAME}"
   echo "****************************************************************************************************************************************************"
-  scan_module "${SCRIPT_DIRECTORY}/docs/java-custom-rules-example" "${PROJECT_VERSION}"
-  scan_module "${SCRIPT_DIRECTORY}/external-reports" "${PROJECT_VERSION}"
-  scan_module "${SCRIPT_DIRECTORY}/its/plugin/tests" "${PROJECT_VERSION}"
-  scan_module "${SCRIPT_DIRECTORY}/its/plugin/plugins/java-extension-plugin" "${PROJECT_VERSION}"
-  scan_module "${SCRIPT_DIRECTORY}/its/ruling" "${PROJECT_VERSION}"
-  scan_module "${SCRIPT_DIRECTORY}/java-checks" "${PROJECT_VERSION}"
-  scan_module "${SCRIPT_DIRECTORY}/java-checks-testkit" "${PROJECT_VERSION}"
-  scan_module "${SCRIPT_DIRECTORY}/java-frontend" "${PROJECT_VERSION}"
-  scan_module "${SCRIPT_DIRECTORY}/java-jsp" "${PROJECT_VERSION}"
-  scan_module "${SCRIPT_DIRECTORY}/java-surefire" "${PROJECT_VERSION}"
-  scan_module "${SCRIPT_DIRECTORY}/java-symbolic-execution" "${PROJECT_VERSION}"
-  scan_module "${SCRIPT_DIRECTORY}/jdt" "${PROJECT_VERSION}"
-  scan_module "${SCRIPT_DIRECTORY}/sonar-java-plugin" "${PROJECT_VERSION}"
+  for module in "${MODULES[@]}"; do
+    local path_to_module="${SCRIPT_DIRECTORY}/${module}"
+    scan_module "${path_to_module}" "${PROJECT_VERSION}"
+  done
 }
 
 get_unified_agent
