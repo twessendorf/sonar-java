93f7c261cf30538dab13415a482b025dbcbe9ff1
Dorian Burihabwa <dorian.burihabwa@sonarsource.com>
2021-11-05 11:48:10 +0100

    Disable effective usage analysis

--- ws_scan.sh
+++ ws_scan.sh
@@ -1,13 +1,8 @@
 #! /usr/bin/env bash
 
-readonly SCRIPT_DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-readonly DEFAULT_MODULES="./"
-
 readonly UNIFIED_AGENT_JAR="wss-unified-agent.jar"
 readonly UNIFIED_AGENT_JAR_URL="https://unified-agent.s3.amazonaws.com/wss-unified-agent.jar"
 readonly WHITESOURCE_SIGNATURE='Signed by "CN=whitesource software inc, O=whitesource software inc, STREET=79 Madison Ave, L=New York, ST=New York, OID.2.5.4.17=10016, C=US"'
-
-readonly JDK_11="./jdk-11/"
 
 
 get_unified_agent() {
@@ -57,49 +52,6 @@ get_project_version() {
   fi
 }
 
-download_jdk_11() {
-  curl \
-    --location \
-    --remote-name \
-    --remote-header-name https://download.java.net/openjdk/jdk11/ri/openjdk-11+28_linux-x64_bin.tar.gz
-  tar xaf openjdk-11+28_linux-x64_bin.tar.gz
-}
-
-scan_module() {
-  local modules="${1}"
-  local version="${2}"
-  local module_name
-  module_name=$(basename "${1}")
-  if [[ ! -d "${JDK_11}" ]]; then
-    download_jdk_11
-  fi
-  JAVA_HOME="$(readlink -f ${JDK_11})"
-  export JAVA_HOME
-  local app_path="${modules}/target/${module_name}-${version}.jar"
-  if [[ -f "${app_path}" ]]; then
-    java --illegal-access=warn -jar wss-unified-agent.jar -c whitesource.properties -appPath "${app_path}" -d "${modules}"
-  else
-    echo "Could not find target jar path: ${app_path}" >&2
-  fi
-  unset JAVA_HOME
-}
-
-scan_multi_module() {
-  local modules="${1}"
-  local app_path="${2}"
-  if [[ ! -d "${JDK_11}" ]]; then
-    download_jdk_11
-  fi
-  JAVA_HOME="$(readlink -f ${JDK_11})"
-  export JAVA_HOME
-  if [[ -f "${app_path}" ]]; then
-    java --illegal-access=warn -jar ${UNIFIED_AGENT_JAR} -c whitesource.properties -appPath "${app_path}" -d "${modules}"
-  else
-    echo "Could not find target jar path: ${app_path}" >&2
-  fi
-  unset JAVA_HOME
-}
-
 scan() {
   WS_PRODUCTNAME=$(get_product_name)
   export WS_PRODUCTNAME
@@ -108,19 +60,15 @@ scan() {
   fi
   export WS_PROJECTNAME="${WS_PRODUCTNAME} ${PROJECT_VERSION%.*}"
   echo "${WS_PRODUCTNAME} - ${WS_PROJECTNAME}"
-  local path_to_jar="${SCRIPT_DIRECTORY}/sonar-java-plugin/target/sonar-java-plugin-${PROJECT_VERSION}.jar"
-  if [[ -z "${MODULES}" ]]; then
-    scan_multi_module DEFAULT_MODULES "${path_to_jar}"
-  else
-    local split_modules
-    IFS="," read -ra split_modules <<< "${MODULES}"
-    for module in "${split_modules[@]}"; do
-      scan_module "${module}" "${PROJECT_VERSION}"
-    done
-  fi
-  echo "MODULES=${MODULES}"
-
+  java -jar ${UNIFIED_AGENT_JAR} -c whitesource.properties
 }
 
-get_unified_agent
-scan
+main() {
+  get_unified_agent
+  scan
+}
+
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+  main
+fi
+
